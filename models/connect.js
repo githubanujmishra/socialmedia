3fd6272fcc103a9998e9fe73b0744db80b1faa6a
githubanujmishra <anujmishra2702@gmail.com>
2024-06-06 00:02:40 +0530

    online db connected

--- models/connect.js
+++ models/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose') ; 
 
 mongoose
-    .connect('mongodb://127.0.0.1:27017/SM')
+    .connect('mongodb+srv://anuj123:anuj123@lakeboycluster.v4ui1it.mongodb.net/SM?retryWrites=true&w=majority&appName=lakeboycluster')
     .then(() => console.log('db connected'))
     .catch((err) => console.log(err.message));
